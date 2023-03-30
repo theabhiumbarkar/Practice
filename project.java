1dd158ffd130fc244f39940e2200901f4ad2f900
Abhishek <abhiumbarkar115@gmail.com>
2023-03-30 13:10:05 +0530

    commit 2

--- project.java
+++ project.java
@@ -1,5 +1,7 @@
 public class project{
     public static void main(String[] args) {
         System.out.println("Welcome ");
+        System.out.println(Hello);
+        System.out.println("Hi");
     }
 }
